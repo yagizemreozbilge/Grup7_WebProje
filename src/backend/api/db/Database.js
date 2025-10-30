d4259470dbc417caa6958aca478af4faa81f6ddf
yagizemreozbilge <yagizozbilge@gmail.com>
2025-10-30 20:21:34 +0300

    create models file and writing Users and UserRoles

---
+++ src/backend/api/db/Database.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+
+let instance = null;
+class Database {
+     constructor() {
+        if(!instance) {
+            this.mongoConnection = null;
+            instance = this;
+        }
+        return instance;
+
+     }
+
+     async connect(options) {
+        try {
+        console.log("DB connecting");
+        let db = await mongoose.connect(options.CONNECTION_STRING);
+        this.mongoConnection = db;
+        console.log("DB connected");
+        } catch(err) {
+            console.error(err);
+            process.exit(1);
+        }
+        
+     }
+}
+
+module.exports = Database;
