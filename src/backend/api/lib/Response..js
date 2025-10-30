03c9df5e5b8afaf87fb25c6dcd6324d1465630e9
yagizemreozbilge <yagizozbilge@gmail.com>
2025-10-30 23:48:59 +0300

    writing config and lib file

---
+++ src/backend/api/lib/Response..js
@@ -0,0 +1,50 @@
+const Enum = require("../config/Enum");
+const CustomError = require("./Error");
+
+
+
+
+
+class Response {
+    constructor() {}
+
+    static successResponse(data,code=200) {
+        return {
+            code,
+            data
+        }
+    }
+
+    static errorResponse(error) {
+  console.error(error);
+  if (error instanceof CustomError) {
+    return {
+      code: error.code,
+      error: {
+        message: error.message,
+        description: error.description
+      }
+    }
+  } else if(error.message.includes("E11000")) {
+
+     return {
+    code: Enum.HTTP_CODES.CONFLICT,
+    error: {
+      message: "Already exists!",
+      description: "Already exists!"
+    }
+  }
+
+  }
+  return {
+    code: Enum?.HTTP_CODES?.INT_SERVER_ERROR ?? 500,
+    error: {
+      message: "Unknown Error!",
+      description: error?.message || String(error)
+    }
+  }
+}
+
+}
+
+module.exports = Response;
