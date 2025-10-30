64c097dca413f214789f757a1e506f2327f4ffab
yagizemreozbilge <yagizozbilge@gmail.com>
2025-10-30 22:58:22 +0300

    create a few files in routes

---
+++ src/backend/api/routes/auditlogs.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const Response = require("../lib/Response");
+const moment = require("moment");
+const AuditLogs = require("../db/models/AuditLogs");
+const router = express.Router();
+
+
+
+router.post("/", async(req,res) =>{
+    try {
+        let body = req.body;
+        let query = {};
+         let skip = body.skip;
+        let limit = body.limit;
+
+        if (typeof body.skip !== "number") {
+            skip = 0;
+        }
+
+        if (typeof body.limit !== "number" || body.limit > 500) {
+            limit = 500;
+        }
+
+        if(body.begin_date && body.end_date) {
+            query.created_at = {
+                $gte: moment(body.begin_date),
+                $lte: moment(body.end_date)
+            }
+        } else {
+             query.created_at = {
+                $gte: moment().subtract(1,"day").startOf("day"),
+                $lte: moment()
+            }
+        }
+        let auditLogs = await AuditLogs.find(query).sort({ created_at: -1 }).skip(skip).limit(limit);
+
+        res.json(Response.successResponse(auditLogs));
+    } catch(err) {
+        let errorResponse = Response.errorResponse(err);
+return res.status(errorResponse.code).json(errorResponse);
+    }
+})
+
+module.exports=router;
