27b1201dda3ed1109358bf9991e29691ffb77dcf
yagizemreozbilge <yagizozbilge@gmail.com>
2025-11-01 13:27:20 +0300

    writing auth.js and updating index.js

--- src/backend/api/config/index.js
+++ src/backend/api/config/index.js
@@ -2,6 +2,12 @@
 require('dotenv').config();
 
 module.exports = {
-  CONNECTION_STRING: process.env.CONNECTION_STRING || 'mongodb://localhost:27017/Grup7_WebProje',
-  PORT: process.env.PORT || 3000,
+  
+  "PORT": process.env.PORT || 3000,
+  "LOG_LEVEL": process.env.LOG_LEVEL || "debug",
+  "CONNECTION_STRING": process.env.CONNECTION_STRING || 'mongodb://localhost:27017/Grup7_WebProje',
+  "JWT": {
+    "SECRET": 122436,
+    "EXPIRE_TIME": !isNaN(parseInt(process.env.TOKEN_EXPIRE_TIME)) ? parseInt(process.env.TOKEN_EXPIRE_TIME) : 24 * 60 * 60 // 86400
+  }
 };
