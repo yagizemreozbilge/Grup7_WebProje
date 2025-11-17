ccda470ce724e07126ae9c37ee3da4c3f6cdc4b4
Gökçe Nur Haymana <haymana.gokce@gmail.com>
2025-11-17 15:23:53 +0300

    Backend güncellemeleri

--- src/backend/api/app.js
+++ src/backend/api/app.js
@@ -1,5 +1,5 @@
-if(process.env.NODE_ENV != "production")
-  require('dotenv').config()
+if (process.env.NODE_ENV != "production")
+    require('dotenv').config()
 
 var createError = require('http-errors');
 var express = require('express');
@@ -7,6 +7,8 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var cors = require('cors');
+var customersRouter = require('./routes/customers');
+var adminRouter = require('./routes/admin');
 
 
 
@@ -19,15 +21,15 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
-app.use(cors()); 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use((req,res,next) => {
-  console.log("Merhaba Dunya!");
-  next();
+app.use((req, res, next) => {
+    console.log("Merhaba Dunya!");
+    next();
 });
 
 app.use('/', require('./routes/index'));
@@ -49,18 +51,18 @@ app.use('/auditlogs', require('./routes/auditlogs'));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
+    next(createError(404));
 });
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
 
 module.exports = app;
